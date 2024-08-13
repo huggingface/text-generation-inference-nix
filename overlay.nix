db99f8364ca1bd5956973f2de9c9c55e3329328d
Nicolas Patry <patry.nicolas@protonmail.com>
2024-08-13 08:13:09 +0200

    python3Packages.exllamav2: init at 0.1.8

--- overlay.nix
+++ overlay.nix
@@ -2,6 +2,8 @@ self: super: {
   python3 = super.python3.override {
     packageOverrides =
       python-self: python-super: with python-self; {
+        exllamav2 = callPackage ./pkgs/python-modules/exllamav2 { };
+
         fbgemm-gpu = callPackage ./pkgs/python-modules/fbgemm-gpu { };
 
         flash-attn = callPackage ./pkgs/python-modules/flash-attn { };
---
+++ pkgs/python-modules/exllamav2/default.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  buildPythonPackage,
+  autoAddDriverRunpath,
+  cmake,
+  git,
+  ninja,
+  packaging,
+  psutil,
+  which,
+  cudaPackages,
+  torch,
+  safetensors,
+  tokenizers,
+  rich,
+}:
+
+buildPythonPackage rec {
+  pname = "exllamav2";
+  version = "0.1.8";
+
+  src = fetchFromGitHub {
+    owner = "turboderp";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-w5LDZi/HyWodqlV3ZUabR5cEBzQMaSmHY5rVVQeMXCs=";
+  };
+
+  stdenv = cudaPackages.backendStdenv;
+
+  buildInputs = with cudaPackages; [
+    cuda_cccl
+    cuda_cudart
+    libcublas
+    libcusolver
+    libcusparse
+    libcurand
+  ];
+
+  nativeBuildInputs = [
+    autoAddDriverRunpath
+    cmake
+    ninja
+    which
+  ];
+
+  dependencies = [
+    torch
+    safetensors
+    tokenizers
+    rich
+  ];
+
+  env = {
+    CUDA_HOME = lib.getDev cudaPackages.cuda_nvcc;
+    TORCH_CUDA_ARCH_LIST = lib.concatStringsSep ";" torch.cudaCapabilities;
+  };
+
+  propagatedBuildInputs = [ torch ];
+
+  # cmake/ninja are used for parallel builds, but we don't want the
+  # cmake configure hook to kick in.
+  dontUseCmakeConfigure = true;
+
+  # We don't have any tests in this package (yet).
+  doCheck = false;
+
+  preBuild = ''
+    export MAX_JOBS=$NIX_BUILD_CORES
+  '';
+
+  # pythonImportsCheck = [ "exllamav2" ];
+
+  meta = with lib; {
+    description = "Inference library for running local LLMs on modern consumer GPUs";
+    license = licenses.mit;
+  };
+}
