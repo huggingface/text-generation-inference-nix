62383dbafb58bd1093ae361691a0614df005a34b
Daniël de Kok <me@danieldk.eu>
2024-08-30 12:01:31 +0000

    treewide: switch from nixpkgs fork for CUDA 12.4 to upstream
    CUDA 12.4 is supported in nixpkgs now.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "tgi development";
 
   inputs = {
-    nixpkgs.url = "github:danieldk/nixpkgs/cuda-12.4";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-compat.url = "github:edolstra/flake-compat";
   };
 
