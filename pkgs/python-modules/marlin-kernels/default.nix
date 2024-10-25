d1801f5d6e6c40aebc3d4c1d27458822443f8b07
Daniël de Kok <me@danieldk.eu>
2024-10-25 16:41:12 +0200

    python3Packages.marlin-kernels: 0.2.0 -> 0.3.0 (#22)
    * python3Packages.marlin-kernels: 0.2.0 -> 0.3.0
    
    * nix fmt

--- pkgs/python-modules/marlin-kernels/default.nix
+++ pkgs/python-modules/marlin-kernels/default.nix
@@ -7,40 +7,64 @@
   cmake,
   ninja,
   which,
+  packaging,
+  setuptools,
+  wheel,
   cudaPackages,
   torch,
 }:
 
+let
+  cutlass = fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "cutlass";
+    rev = "refs/tags/v3.5.1";
+    hash = "sha256-sTGYN+bjtEqQ7Ootr/wvx3P9f8MCDSSj3qyCWjfdLEA=";
+  };
+in
 buildPythonPackage rec {
   pname = "marlin-kernels";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "danieldk";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-aTLRpU9/hCSc0gDrsSGOIjMITkBOo1fC/8y4MqyDk1o=";
+    hash = "sha256-Fw/pWQChJ/bB3MfRTmUYhklJHVvgS5cMGIOcqHPZed4=";
   };
 
+  patches = [
+    ./setup.py-nix-support-respect-cmakeFlags.patch
+  ];
+
   stdenv = cudaPackages.backendStdenv;
+
+  nativeBuildInputs = with cudaPackages; [
+    autoAddDriverRunpath
+    cmake
+    cuda_nvcc
+    ninja
+    which
+  ];
+
+  build-system = [
+    packaging
+    setuptools
+    wheel
+  ];
 
   buildInputs = with cudaPackages; [
     cuda_cccl
     cuda_cudart
+    cuda_nvtx
     libcublas
     libcusolver
     libcusparse
   ];
 
-  nativeBuildInputs = [
-    autoAddDriverRunpath
-    cmake
-    ninja
-    which
-  ];
-
   env = {
     CUDA_HOME = "${lib.getDev cudaPackages.cuda_nvcc}";
+    TORCH_CUDA_ARCH_LIST = lib.concatStringsSep ";" torch.cudaCapabilities;
   };
 
   propagatedBuildInputs = [ torch ];
@@ -48,6 +72,8 @@ buildPythonPackage rec {
   # cmake/ninja are used for parallel builds, but we don't want the
   # cmake configure hook to kick in.
   dontUseCmakeConfigure = true;
+
+  cmakeFlags = [ (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_CUTLASS" "${lib.getDev cutlass}") ];
 
   # We don't have any tests in this package (yet).
   doCheck = false;
