fd3fdf8651e4c1ffe89e6d7a1b9de27b99abc43f
Daniël de Kok <me@danieldk.eu>
2024-11-11 09:19:51 +0100

    python3Packages.marlin-kernels: 0.3.1 -> 0.3.3 (#26)

--- pkgs/python-modules/marlin-kernels/default.nix
+++ pkgs/python-modules/marlin-kernels/default.nix
@@ -24,13 +24,13 @@ let
 in
 buildPythonPackage rec {
   pname = "marlin-kernels";
-  version = "0.3.1";
+  version = "0.3.3";
 
   src = fetchFromGitHub {
     owner = "danieldk";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-2XEwbJCLyAOlxHEXwDN1dUIgA99Up6zMSVEsOynfVoI=";
+    hash = "sha256-kydIxV+0L8hgu0cUH5ugct7jVISTKn8EUnTR6mVkLEQ=";
   };
 
   patches = [
